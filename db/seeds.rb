cb4f97ecb2f381abf9d5b95c12607a8c7543355c
aladac <adam.ladachowski@gmail.com>
2021-11-22 19:03:37 +0100

    Price seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 movies = YAML.load_file('db/seeds/movies.yml')
+prices = YAML.load_file('db/seeds/prices.yml')
 
 movies.each do |movie_attributes|
   Movie.create(movie_attributes)
+end
+
+prices.each do |price_attributes|
+  Price.create(price_attributes)
 end
